61575ed9614532c005bb5eac91255c2e08e68508
Asya19 <asya.kisko@yandex.ru>
2022-05-22 18:50:13 +0300

    feat: add 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,25 @@
+const {stdin, stdout, stderr} = process;
+
+const fs = require('fs');
+const path = require('path');
+
+const writStream = fs.createWriteStream(path.join(__dirname, 'test.txt'));
+stdout.write('Введите текст.\nЧтобы закончит ввод, введите exit\n');
+
+stdin.on('data', data => {
+  const text = data.toString();
+  if (text.trim() === 'exit') process.exit();
+  
+  writStream.write(`${text}`, 
+    err => {
+      if (err) throw err;
+    }
+  );
+});
+
+process.on('exit', code => {
+  if (code === 0)
+    stdout.write('\nСпасибо за проверку, пока!\n');
+  else
+    stderr.write(`Ошибка с кодом ${code}`);
+});
